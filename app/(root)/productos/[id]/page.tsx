f680ec013246c30de2959f01f0d94b9c5afd8812
Briell Quintana <brielluh@gmail.com>
2025-06-14 09:34:31 -0400

    fix gap on product details page

--- app/(root)/productos/[id]/page.tsx
+++ app/(root)/productos/[id]/page.tsx
@@ -24,7 +24,7 @@ const page = async ({ params }: Props) => {
   const categories = await productsQuery(product.category);
 
   return (
-    <div className="flex flex-col items-center justify-center gap-20">
+    <div className="flex flex-col items-center justify-center gap-10">
       <div>
         <div className="grid place-content-center">
           <HeaderLink link={true} href="/productos">
