a611b1c5131cb46f5659cc34078c208fbdf13a7a
Briell Quintana <brielluh@gmail.com>
2025-06-14 23:11:09 -0400

    implement layout height

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,7 +9,7 @@ const HomeLayout = ({ children }: Props) => {
   return (
     <div className="relative flex h-screen flex-col">
       <Navbar />
-      <main className="container mx-auto max-w-7xl flex-grow px-6 pt-16">
+      <main className="container mx-auto min-h-screen max-w-7xl flex-grow px-6 pt-16">
         {children}
       </main>
       <footer className="flex w-full items-center justify-center py-3">
